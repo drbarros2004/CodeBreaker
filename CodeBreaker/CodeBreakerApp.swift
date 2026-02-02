44b83095efef2d9fca6fd8fbbac83751c4503dcb
drbarros2004 <drbarros2004@gmail.com>
2026-02-02 18:29:09 -0300

    Initial Commit

---
+++ CodeBreaker/CodeBreakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBreakerApp.swift
+//  CodeBreaker
+//
+//  Created by Daniel Barros on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CodeBreakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
